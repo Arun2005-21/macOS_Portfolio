91f81f2d68fc0ac76aadc4fba1f186918b061d04
Arun2005-21 <altarun90@gmail.com>
2026-02-14 22:30:09 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import {resolve, dirname} from "path";
+// import { fileURLToPath } from 'url';
+
+// // ./../components/something
+// // @components/component
+// export default defineConfig({
+//   plugins: [react(),tailwindcss()],
+//   resolve : {
+//     alias:{
+//       "#components":resolve(
+//         dirname(fileURLToPath(import.meta.url)),
+//         "src/component",
+//       ),
+//       "#constants":resolve(
+//         dirname(fileURLToPath(import.meta.url)),
+//         "src/constants",
+//       ),
+
+//       "#store":resolve
+//       (dirname(fileURLToPath(import.meta.url)),
+//       "src/store"),
+//       "#hoc":resolve(dirname(fileURLToPath(import.meta.url)),"src/hoc"),
+//       "#windows":resolve(
+//         dirname(fileURLToPath(import.meta.url)),"src/windows",
+//     ),
+//     }
+//   }
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const root = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "#components": resolve(root, "src/components"),
+      "#constants": resolve(root, "src/constants"),
+      "#store": resolve(root, "src/store"),
+      "#hoc": resolve(root, "src/hoc"),
+      "#windows": resolve(root, "src/windows"),
+    },
+  },
+});
