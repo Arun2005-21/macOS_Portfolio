7cae45f2b11a2ac9155c9c97edf43a9324cb3d39
Arun2005-21 <altarun90@gmail.com>
2026-02-18 23:51:26 +0530

    fix: windows index imports for vercel build

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,7 +2,7 @@ import Terminal  from "./Terminal";
 import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
-import Text from "./text";
+import Text from "./Text";
 import Image from "./Image";
 import Contact from "./Contact";
 import Photos from "./Photos";
